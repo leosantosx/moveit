7b944046083a5f47dfd296c109249a7b024084f5
unknown <leonardosanttos0077@gmail.com>
2021-02-26 19:09:23 -0300

    finalizando projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,7 @@
-import { ChallengesProvider } from '../contexts/ChallengeContext'
-
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
